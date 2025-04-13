92eb608cc0140669395fc3527bfe365e9c572522
sudhanshu <sudhanshuarya305@gmail.com>
2025-04-13 18:44:13 +0530

    Update Page Table

--- src/memory/paging/paging.c
+++ src/memory/paging/paging.c
@@ -1,4 +1,5 @@
 #include "paging.h"
+#include "status.h"
 #include "memory/heap/kheap.h"
 
 static uint32_t* current_directory = 0;
@@ -33,4 +34,43 @@ void paging_switch(uint32_t* directory) {
 
 uint32_t* paging_4gb_chunk_get_directory(struct paging_4gb_chunk* chunk) {
     return chunk->directory_entry;
+}
+
+bool paging_is_aligned(void* addr) {
+    return ((uint32_t)addr % PAGING_PAGE_SIZE) == 0;
+}
+
+// Takes Virtual Address and calculate the directory index and page table index
+int paging_get_indexes(void* virtual_addres, uint32_t* directory_index_out, uint32_t* table_index_out) {
+    int res = 0;
+    if(!paging_is_aligned(virtual_addres)) { // Checking if the address is valid for 4096
+        res = -EINVARG;
+        goto out;
+    }
+
+    *directory_index_out = ((uint32_t)virtual_addres / (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE));
+    *table_index_out = ((uint32_t)virtual_addres % (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE) / PAGING_PAGE_SIZE);
+
+    out:
+        return res;
+}
+
+int paging_set(uint32_t* directory, void* virtual_address, uint32_t val) { // val contains Physical address along with flags
+    if(!paging_is_aligned(virtual_address)) {
+        return -EINVARG;
+    }
+
+    uint32_t directory_index = 0;
+    uint32_t table_index = 0;
+    int res = paging_get_indexes(virtual_address, &directory_index, &table_index);
+    if(res < 0) {
+        return res;
+    }
+    
+    uint32_t entry = directory[directory_index];
+    uint32_t* table = (uint32_t*)(entry & 0xfffff000); // Extracting top 20 Bits which has Page Table 4-kb aligned Address
+
+    table[table_index] = val;
+
+    return 0;
 }
